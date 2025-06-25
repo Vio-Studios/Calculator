c5c56229776c1af7473489d49706eb3c1fbd6047
Violetz <m001747@permatapintar.ukm.edu.my>
2025-06-25 02:04:19 +0000

    test 2

--- calculator.cs
+++ calculator.cs
@@ -1,17 +1,48 @@
 using System;
-					
+
 public class Program
-{		
-	public static void Main()
-	{
-		Console.Write("Enter first number :");
-		int num1 = Convert.ToInt32(Console.ReadLine());
-		
-		Console.Write("Enter second number: ");
-		int num2= Convert.ToInt32(Console.ReadLine());
-		
-		Console.Write("Enter operation: ");
-		string operation =
-		
-	}
+{
+    public static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter operation (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+        int result = 0;
+
+        if (operation == "+")
+        {
+            result = num1 + num2;
+        }
+        else if (operation == "-")
+        {
+            result = num1 - num2;
+        }
+        else if (operation == "*")
+        {
+            result = num1 * num2;
+        }
+        else if (operation == "/")
+        {
+            if (num2 != 0)
+                result = num1 / num2;
+            else
+            {
+                Console.WriteLine("Cannot divide by zero!");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation.");
+            return;
+        }
+
+        Console.WriteLine("The result is: " + result);
+    }
 }
