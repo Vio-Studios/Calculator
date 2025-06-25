50635de30fa101b6bbca954a0b3665d48b63d6a6
Violetz <m001747@permatapintar.ukm.edu.my>
2025-06-25 02:02:22 +0000

    Test 1

---
+++ calculator.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{		
+	public static void Main()
+	{
+		Console.Write("Enter first number :");
+		int num1 = Convert.ToInt32(Console.ReadLine());
+		
+		Console.Write("Enter second number: ");
+		int num2= Convert.ToInt32(Console.ReadLine());
+		
+		Console.Write("Enter operation: ");
+		string operation =
+		
+	}
+}
